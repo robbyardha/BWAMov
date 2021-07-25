9728dea18db88d209ed186859407be412a0722d4
robbyardha <robbyardha@gmail.com>
2021-07-26 01:05:50 +0700

    first

---
+++ app/src/main/java/com/ardhacodes/bwamov/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.ardhacodes.bwamov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
