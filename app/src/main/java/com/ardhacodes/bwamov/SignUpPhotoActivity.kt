73a32df8dbf3cc050d5a6c013863bc618cbaa22c
robbyardha <robbyardha@gmail.com>
2021-07-29 01:36:56 +0700

    add activity signup photo

---
+++ app/src/main/java/com/ardhacodes/bwamov/SignUpPhotoActivity.kt
@@ -0,0 +1,11 @@
+package com.ardhacodes.bwamov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpPhotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_photo)
+    }
+}
